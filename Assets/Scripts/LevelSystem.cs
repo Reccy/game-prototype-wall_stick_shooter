958af3c27d1c86ea8c038f270106b3df85d322e1
Reccy <aaronmeaney@hotmail.com>
2016-08-03 17:35:36 +0100

    Added debug command to switch scenes.

---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelSystem : MonoBehaviour {
+
+    //Basic debug level system. Obviously real game will have a better system ;)
+    void Update()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            if(Input.GetKeyDown(i.ToString()))
+            {
+                if(i < SceneManager.sceneCountInBuildSettings)
+                {
+                    SceneManager.LoadScene(i);
+                    Debug.Log("Loading Scene: " + i);
+                }
+                else
+                {
+                    Debug.Log("Scene " + i + " not in this build!");
+                }
+            }
+        }
+    }
+}
