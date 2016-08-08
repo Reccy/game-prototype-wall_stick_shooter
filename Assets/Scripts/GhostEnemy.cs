4abc93950975fb8e9cdcaa3a9a2056b583e81dc1
Reccy <aaronmeaney@hotmail.com>
2016-08-08 15:21:41 +0100

    Added ghost enemy pattern.

---
+++ Assets/Scripts/GhostEnemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostEnemy : MonoBehaviour {
+
+    CircleCollider2D cCol;
+    SpriteRenderer sRend;
+
+    void Awake()
+    {
+        cCol = GetComponent<CircleCollider2D>();
+        sRend = GetComponent<SpriteRenderer>();
+        cCol.enabled = false;
+    }
+
+	void Start () {
+        StartCoroutine(TurnIntoEnemy());
+	}
+	
+	IEnumerator TurnIntoEnemy()
+    {
+        yield return new WaitForSeconds(2);
+        sRend.color = Color.red;
+        cCol.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerManager : MonoBehaviour {
+
+    public GameObject ghostEnemy;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        yield return new WaitForSeconds(2f);
+        for (int i = 0; i < 3; i++ )
+        {
+            Instantiate(ghostEnemy, GameObject.FindGameObjectWithTag("Player").transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(4f);
+        }
+        GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().SpawnNewBulletPattern();
+        Destroy(this.gameObject);
+    }
+}
